d0910878e668beb108f25737ef49c676a0d5970b
xin <git@ironshark.org>
2024-04-29 18:53:19 -0400

    Rebuild:  213   2024-04-29 18:42:22

--- Shared/System-Tools/waybar.nix
+++ Shared/System-Tools/waybar.nix
@@ -4,7 +4,9 @@
     enable = true;
   };
 
-  home.file."waybar-config" = {
+  home.file = {
+
+    "waybar-config" = {
     target = ".config/waybar/config.jsonc";
     text = ''
       {
@@ -14,7 +16,7 @@
 
           "modules-left": ["sway/workspaces", "sway/mode"],
           "modules-center": ["sway/window"],
-          "modules-right": ["tray", "custom/spotify", "custom/weather", "custom/mail", "custom/storage", "backlight", "pulseaudio", "network", "idle_inhibitor", "battery", "clock"],
+          "modules-right": ["tray", "pulseaudio", "network", "idle_inhibitor", "clock"],
           "sway/mode": {
               "format": " {}"
           },
@@ -68,5 +70,76 @@
           }
       }
     '';
+    };
+
+    "waybar-css" = {
+      target = ".config/waybar/config.jsonc";
+      text = ''
+        * {
+            border:        none;
+            border-radius: 0;
+            font-family:   Sans;
+            font-size:     15px;
+            box-shadow:    none;
+            text-shadow:   none;
+            transition-duration: 0s;
+        }
+
+        window {
+            color:      rgba(217, 216, 216, 1);
+            background: rgba(35, 31, 32, 0.00);
+        }
+
+        window#waybar.solo {
+            color:      rgba(217, 216, 216, 1);
+            background: rgba(35, 31, 32, 0.85);
+        }
+
+        #workspaces {
+            margin: 0 5px;
+        }
+
+        #workspaces button {
+            padding:    0 5px;
+            color:      rgba(217, 216, 216, 0.4);
+        }
+
+        #workspaces button.visible {
+            color:      rgba(217, 216, 216, 1);
+        }
+
+        #workspaces button.focused {
+            border-top: 3px solid rgba(217, 216, 216, 1);
+            border-bottom: 3px solid rgba(217, 216, 216, 0);
+        }
+
+        #workspaces button.urgent {
+            color:      rgba(238, 46, 36, 1);
+        }
+
+        #mode, #battery, #cpu, #memory, #network, #pulseaudio, #idle_inhibitor, #backlight, #custom-storage, #custom-spotify, #custom-weather, #custom-mail {
+            margin:     0px 6px 0px 10px;
+            min-width:  25px;
+        }
+
+        #clock {
+            margin:     0px 16px 0px 10px;
+            min-width:  140px;
+        }
+
+        #battery.warning {
+           color:       rgba(255, 210, 4, 1);
+        }
+
+        #battery.critical {
+            color:      rgba(238, 46, 36, 1);
+        }
+
+        #battery.charging {
+            color:      rgba(217, 216, 216, 1);
+        }
+      '';
+    };
+
   };
 }
