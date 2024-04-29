de1c11022ed2f1153b3ce7e9f67f9476fdee16a8
xin <git@ironshark.org>
2024-04-29 18:42:24 -0400

    Rebuild:  212   2024-04-29 15:12:42

--- Shared/System-Tools/waybar.nix
+++ Shared/System-Tools/waybar.nix
@@ -2,70 +2,71 @@
 
   programs.waybar = {
     enable = true;
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [
-          "DP-1"
-        ];
+  };
 
-        "modules-left": ["sway/workspaces", "sway/mode"],
-        "modules-center": ["sway/window"],
-        "modules-right": ["tray", "pulseaudio", "network", "idle_inhibitor", "clock"],
+  home.file."waybar-config" = {
+    target = ".config/waybar/config.jsonc";
+    text = ''
+      {
+          "layer": "bottom",
+          "position": "top",
+          "height": 30,
+
+          "modules-left": ["sway/workspaces", "sway/mode"],
+          "modules-center": ["sway/window"],
+          "modules-right": ["tray", "custom/spotify", "custom/weather", "custom/mail", "custom/storage", "backlight", "pulseaudio", "network", "idle_inhibitor", "battery", "clock"],
           "sway/mode": {
-            "format": " {}"
+              "format": " {}"
           },
           "sway/workspaces": {
-            "format": "{name}",
-            "disable-scroll": true
+              "format": "{name}",
+              "disable-scroll": true
           },
           "sway/window": {
-            "max-length": 80,
-            "tooltip": false
+              "max-length": 80,
+              "tooltip": false
           },
           "clock": {
-            "format": "{:%a %d %b %H:%M}",
-            "tooltip": false
+              "format": "{:%a %d %b %H:%M}",
+              "tooltip": false
           },
           "network": {
-            "format": "{icon}",
-            "format-alt": "{ipaddr}/{cidr} {icon}",
-            "format-alt-click": "click-right",
-            "format-icons": {
-              "wifi": ["", "" ,""],
-              "ethernet": [""],
-              "disconnected": [""]
-            },
-            "on-click": "termite -e nmtui",
-            "tooltip": false
+              "format": "{icon}",
+              "format-alt": "{ipaddr}/{cidr} {icon}",
+              "format-alt-click": "click-right",
+              "format-icons": {
+                  "wifi": ["", "" ,""],
+                  "ethernet": [""],
+                  "disconnected": [""]
+              },
+              "on-click": "termite -e nmtui",
+          "tooltip": false
           },
           "pulseaudio": {
-            "format": "{icon}",
-            "format-alt": "{volume} {icon}",
-            "format-alt-click": "click-right",
-            "format-muted": "",
-            "format-icons": {
-              "phone": [" ", " ", " ", " "],
-              "default": ["", "", "", ""]
-            },
-            "scroll-step": 10,
-            "on-click": "pavucontrol",
-            "tooltip": false
+              "format": "{icon}",
+              "format-alt": "{volume} {icon}",
+              "format-alt-click": "click-right",
+              "format-muted": "",
+              "format-icons": {
+                  "phone": [" ", " ", " ", " "],
+                  "default": ["", "", "", ""]
+              },
+              "scroll-step": 10,
+              "on-click": "pavucontrol",
+              "tooltip": false
           },
           "idle_inhibitor": {
-            "format": "{icon}",
-            "format-icons": {
-              "activated": "",
-              "deactivated": ""
-            },
-            "tooltip": false
+              "format": "{icon}",
+              "format-icons": {
+                  "activated": "",
+                  "deactivated": ""
+              },
+              "tooltip": false
           },
           "tray": {
-            "icon-size": 18
-          };
-        };
-    };
+              "icon-size": 18
+          }
+      }
+    '';
   };
 }
