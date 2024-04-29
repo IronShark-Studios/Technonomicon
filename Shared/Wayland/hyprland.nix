e9e8359da3268f7eeb20a1ebb2776ca2713f7a83
xin <git@ironshark.org>
2024-04-28 20:43:18 -0400

    Rebuild:  179   2024-04-28 20:33:36

--- Shared/Wayland/hyprland.nix
+++ Shared/Wayland/hyprland.nix
@@ -10,7 +10,7 @@
 
         # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
-        exec-once = waybar
+        exec-once = waybar & waypaper --restore
 
         # Source a file (multi-file configs)
         # source = ~/.config/hypr/myColors.conf
