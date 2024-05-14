0125ca715548ef6bce36daa2cf9cc57c4929ff54
xin <git@ironshark.org>
2024-05-14 12:20:22 -0400

    Rebuild:  267   2024-05-08 10:04:26

--- Shared/Terminal/ZSH/zsh.nix
+++ Shared/Terminal/ZSH/zsh.nix
@@ -6,6 +6,18 @@
     autocd = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
 
     history.ignoreAllDups = true;
     historySubstringSearch.enable = true;
