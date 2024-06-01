d4b3eba9e0ea3a2d57572c8a13da76071f49c5ca
xin <git@ironshark.org>
2024-06-01 17:32:52 -0400

    Rebuild:  334   2024-06-01 14:56:35

--- Shared/Terminal/ZSH/zsh.nix
+++ Shared/Terminal/ZSH/zsh.nix
@@ -2,10 +2,10 @@
 
   programs.zsh = {
     enable = true;
-
     autocd = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     plugins = [
       {
         name = "zsh-nix-shell";
@@ -35,35 +35,35 @@
       bindkey -a 'm' vi-backward-char
       printf '\n%.0s' {1..100}
 
-zle-line-init() {
-  emulate -L zsh
+      zle-line-init() {
+        emulate -L zsh
 
-  [[ $CONTEXT == start ]] || return 0
+        [[ $CONTEXT == start ]] || return 0
 
-  while true; do
-    zle .recursive-edit
-    local -i ret=$?
-    [[ $ret == 0 && $KEYS == $'\4' ]] || break
-    [[ -o ignore_eof ]] || exit 0
-  done
+        while true; do
+          zle .recursive-edit
+          local -i ret=$?
+          [[ $ret == 0 && $KEYS == $'\4' ]] || break
+          [[ -o ignore_eof ]] || exit 0
+        done
 
-  local saved_prompt=$PROMPT
-  local saved_rprompt=$RPROMPT
-  PROMPT='󰜵 '
-  RPROMPT=''\''
-      zle .reset-prompt
-      PROMPT=$saved_prompt
-        RPROMPT=$saved_rprompt
+        local saved_prompt=$PROMPT
+        local saved_rprompt=$RPROMPT
+        PROMPT='󰜵 '
+        RPROMPT=''\''
+            zle .reset-prompt
+            PROMPT=$saved_prompt
+              RPROMPT=$saved_rprompt
 
-          if (( ret )); then
-            zle .send-break
-          else
-            zle .accept-line
-              fi
-              return ret
-  }
+                if (( ret )); then
+                  zle .send-break
+                else
+                  zle .accept-line
+                    fi
+                    return ret
+      }
 
-    zle -N zle-line-init
+          zle -N zle-line-init
     '';
 };
 }
