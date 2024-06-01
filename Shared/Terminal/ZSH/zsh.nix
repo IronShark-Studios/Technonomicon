498c643fed0a555533874f61df2fec7c99a6cf21
xin <git@ironshark.org>
2024-06-01 18:11:20 -0400

    Rebuild:  334   2024-06-01 14:56:35

--- Shared/Terminal/ZSH/zsh.nix
+++ Shared/Terminal/ZSH/zsh.nix
@@ -32,18 +32,6 @@
 
       printf '\n%.0s' {1..100}
 
-      HISTSIZE=3000
-      HISTFILE=~/.zsh_history
-      SAVEHIST=$HISTSIZE
-      HISTDUP=erase
-      setopt appendhistory
-      setopt sharedhistory
-      setopt hist_ignore_space
-      setopt hist_ignore_all_dups
-      setopt hist_save_no_dups
-      setopt hist_ignore_dups
-      setopt hist_find_no_dups
-
       zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
       zstyle ':completion:*' list-color "''\${(s.:.)LS_COLORS}"
       zstyle ':completion:*' menu no
