1367f8b70ca6bbd9d26db13a2862ab9a9f9cd363
xin <git@ironshark.org>
2024-06-03 19:12:13 -0400

    Rebuild:  401   2024-06-02 16:03:36

--- Thanatos/Home-Manager/test-sym-link.nix
+++ Thanatos/Home-Manager/test-sym-link.nix
@@ -2,7 +2,7 @@
 
   home.file = {
     "test-sym-link.org" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/xin/Projects/Technonomicon/Thanatos/Home-Manager/test-sym-link.org"
+      source = config.lib.file.mkOutOfStoreSymlink "/home/xin/Projects/Technonomicon/Thanatos/Home-Manager/test-sym-link.org";
     };
   };
 }
