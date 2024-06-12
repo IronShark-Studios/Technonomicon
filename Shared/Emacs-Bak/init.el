c5824933c28b17ca0c554c9a59e25717962e64a5
xin <git@ironshark.org>
2024-06-12 14:54:25 -0400

    moves todos and visuals vars into spacemacs layer

--- Shared/Emacs-Bak/init.el
+++ Shared/Emacs-Bak/init.el
@@ -1,7 +1,10 @@
 
 
+(use-package rg
+  :config
+  (global-set-key (kbd "C-s") #'rg-menu))
 
-(use-package helm-org-ql)
+
 
 (use-package evil
   :init
@@ -22,6 +25,11 @@
 
   (add-hook 'with-editor-mode-hook 'evil-insert-state))
 
+
+
+
+
+(use-package helm-org-ql)
 
 (use-package helm-company
   :after company
@@ -61,52 +69,6 @@
 
 
 
-;; (add-hook 'org-mode-hook 'Tn/org-font-setup)
-
-;; (defun Tn/org-font-setup ()
-;; ;; This is magic code that changes the font of non-heading bullet point lists.
-;; (font-lock-add-keywords 'org-mode
-;;                         '(("^ *\\([-]\\) "
-;;                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-;; (dolist (face '((org-level-1 . "Azure3")
-;;                 (org-level-2 . "Azure3")
-;;                 (org-level-3 . "Azure3")
-;;                 (org-level-4 . "Azure3")
-;;                 (org-level-5 . "Azure3")
-;;                 (org-level-6 . "Azure3")
-;;                 (org-level-7 . "Azure3")
-;;                 (org-level-8 . "Azure3")))
-;;   (set-face-attribute (car face) nil :font "Iosevka"
-;;                       :weight 'regular :height 1.3
-;;                       :foreground (cdr face)))
-
-;; (set-face-attribute 'org-link nil    :foreground "cyan" :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-tag nil     :height 0.9 :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-block nil    :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-table nil    :foreground "dark cyan" :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-formula nil  :foreground "dark cyan" :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-code nil     :foreground "SpringGreen3"
-;;                     :weight 'semi-bold :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-verbatim nil :foreground "SpringGreen3"
-;;                     :weight 'semi-bold :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-;; (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-;; (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
-
-;; (setq org-priority-faces '((?A . (:foreground "medium spring green" :weight 'bold
-;;                                               :inherit 'fixed-pitch))
-;;                            (?B . (:foreground "deep sky blue" :weight 'bold
-;;                                               :inherit 'fixed-pitch))
-;;                            (?C . (:foreground "blue violet" :weight 'bold
-;;                                               :inherit 'fixed-pitch))
-;;                            (?D . (:foreground "dim grey" :weight 'bold
-;;                                               :inherit 'fixed-pitch))
-;;                            (?E . (:foreground "dark red" :weight 'bold
-;;                                               :inherit 'fixed-pitch))))
 
 
 
@@ -145,10 +107,6 @@
       org-archive-location (format "~/Ferronomicon/\%s/\%s-archvie.org::datetree/" (Tn/current-year) (Tn/current-year)))
 
 (use-package org
-
-:bind
-(("C-c l" . Tn/org-link-hydra/body)
- ("C-c h" . Tn/org-heading-actions-hydra/body))
 
 :config
 (org-babel-do-load-languages
@@ -1801,7 +1759,6 @@ _t_: Tobey Time    _d_: Driving
                                          :order 9))))))))))
 
 
-(use-package org-ql)
 
 (setq-default org-icalendar-include-todo t)
 
@@ -1824,18 +1781,3 @@ _t_: Tobey Time    _d_: Driving
       holiday-oriental-holidays nil
       holiday-other-holidays nil
       org-agenda-include-diary t)
-
-(use-package scad-mode)
-
-
-(use-package ag)
-
-(use-package rg
-  :config
-  (global-set-key (kbd "C-s") #'rg-menu))
-
-(use-package graphviz-dot-mode)
-
-(use-package dirvish
-  :config
-  dirvish-override-dired-mode)
--- Shared/Emacs/Personal-Layers/Extended-Org/config.el
+++ Shared/Emacs/Personal-Layers/Extended-Org/config.el
@@ -28,17 +28,6 @@
               ("CANCELLED" :foreground "dim gray" :weight bold))))
 
 
-(add-hook 'org-mode-hook 'Tn/org-mode-setup)
-(add-hook 'before-save-hook 'Tn/org-set-last-modified)
-(add-hook 'org-capture-mode-hook 'evil-hybrid-state)
-(add-hook 'org-log-buffer-setup-hook 'evil-hybrid-state)
-
-(advice-add 'org-ctrl-c-ctrl-c  :after #'save-buffer)
-(advice-add 'org-deadline       :after #'save-buffer)
-(advice-add 'org-schedule       :after #'save-buffer)
-(advice-add 'org-store-log-note :after #'save-buffer)
-(advice-add 'org-store-log-note :after #'org-cycle)
-
 ;;; Org-Appear
 
 (setq org-appear-trigger 'manual
@@ -51,12 +40,31 @@
       org-link-descriptive t
       org-appear-autosubmarkers t)
 
-  (add-hook 'org-mode-hook (lambda ()
-                             (add-hook 'evil-hybrid-state-entry-hook
-                                       #'org-appear-manual-start
-                                       nil
-                                       t)
-                             (add-hook 'evil-hybrid-state-exit-hook
-                                       #'org-appear-manual-stop
-                                       nil
-                                       t)))
+(add-hook 'org-mode-hook (lambda ()
+                           (add-hook 'evil-hybrid-state-entry-hook
+                                     #'org-appear-manual-start
+                                     nil
+                                     t)
+                           (add-hook 'evil-hybrid-state-exit-hook
+                                     #'org-appear-manual-stop
+                                     nil
+                                     t)))
+
+
+;;; Org Related Keybindings, Hooks, and Advice
+(define-key org-mode-map (kbd "C-c l") 'Tn/org-link-hydra/body)
+(define-key org-mode-map (kbd "C-c h") 'Tn/org-heading-actions-hydra/body)
+;; (define-key org-mode-map (kbd "C-:") 'helm-company)
+
+
+(add-hook 'org-mode-hook 'Tn/org-mode-setup)
+(add-hook 'before-save-hook 'Tn/org-set-last-modified)
+(add-hook 'org-capture-mode-hook 'evil-hybrid-state)
+(add-hook 'org-log-buffer-setup-hook 'evil-hybrid-state)
+
+
+(advice-add 'org-ctrl-c-ctrl-c  :after #'save-buffer)
+(advice-add 'org-deadline       :after #'save-buffer)
+(advice-add 'org-schedule       :after #'save-buffer)
+(advice-add 'org-store-log-note :after #'save-buffer)
+(advice-add 'org-store-log-note :after #'org-cycle)
