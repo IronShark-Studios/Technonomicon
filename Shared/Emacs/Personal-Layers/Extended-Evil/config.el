392ab3f1b2d4534ef45382da243d14cfbfb34c96
xin <git@ironshark.org>
2024-06-13 13:37:17 -0400

    Rebuild:  569   2024-06-13 13:25:33

--- Shared/Emacs/Personal-Layers/Extended-Evil/config.el
+++ Shared/Emacs/Personal-Layers/Extended-Evil/config.el
@@ -9,13 +9,13 @@
 
   (add-hook 'evil-normal-state-hook
             (lambda ()
-              (local-set-key [C-h] 'help)
-              (local-set-key [S-<escape>] 'Tn/evil-normal-and-save)))
+              (define-key evil-normal-state-map (kbd "C-h") 'help)
+              (define-key evil-normal-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)
+              ))
 
   (add-hook 'evil-hybrid-state-hook
             (lambda ()
-              (local-set-key [S-<escape>] 'Tn/evil-normal-and-save)))
-
+              (define-key evil-hybrid-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)))
 
 
 ;;; Closing Paren to Eval Statement
