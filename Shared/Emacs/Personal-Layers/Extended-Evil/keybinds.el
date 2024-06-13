a8bfcf5d7318c4e860876a6b3376344ffec8d853
xin <git@ironshark.org>
2024-06-13 15:24:47 -0400

    Rebuild:  590   2024-06-13 15:15:36

--- Shared/Emacs/Personal-Layers/Extended-Evil/keybinds.el
+++ Shared/Emacs/Personal-Layers/Extended-Evil/keybinds.el
@@ -1,12 +1,16 @@
-(global-set-key (kbd "C-s") #'rg-menu)
+;;; This file must be refrenced from within the user config
+;;; section of the main spacemacs file
+
+
+(global-set-key (kbd "C-s") 'rg-menu)
 (global-set-key (kbd "C-x C-s") 'Tn/save-all-buffers)
 (global-set-key (kbd "C-c !")  'spacemacs/Tn/helm-actions-transient-state/body)
 
 (eval-after-load 'flycheck
-                 '(define-key flycheck-mode-map (kbd "C-:") 'helm-flyspell-correct))
+                 '(global-set-key (kbd "C-:") 'helm-flyspell-correct))
 
 (eval-after-load 'flyspell
-                 '(define-key flyspell-mode-map (kbd "C-;") 'helm-flycheck))
+  '(global-set-key (kbd "C-;") 'helm-flycheck))
 
 
 
@@ -15,13 +19,16 @@
 
 (define-key evil-normal-state-map (kbd "C-h") 'help)
 
-(define-key evil-normal-state-map (kbd "S-n") 'Tn/evil-pg-down-and-center)
-(define-key evil-normal-state-map (kbd "S-e") 'Tn/evil-pg-up-and-center)
+(define-key evil-normal-state-map (kbd "N") 'Tn/evil-pg-down-and-center)
 (define-key evil-normal-state-map (kbd "n") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "E") 'Tn/evil-pg-up-and-center)
 (define-key evil-normal-state-map (kbd "e") 'evil-previous-visual-line)
-(define-key evil-normal-state-map (kbd "S-i") 'Tn/evil-center-page)
+(define-key evil-normal-state-map (kbd "I") 'Tn/evil-center-page)
 (define-key evil-normal-state-map (kbd "i") 'avy-goto-subword-1)
 (define-key evil-normal-state-map (kbd "O") 'join-line)
 (define-key evil-normal-state-map (kbd "o") 'harpoon-quick-menu-hydra)
 (define-key evil-normal-state-map (kbd "/") 'helm-swoop)
 (define-key evil-normal-state-map (kbd "?") 'helm-regexp)
+
+(define-key evil-normal-state-map (kbd "<up>") 'evil-previous-visual-line)
+(define-key evil-normal-state-map (kbd "<down>") 'evil-next-visual-line)
