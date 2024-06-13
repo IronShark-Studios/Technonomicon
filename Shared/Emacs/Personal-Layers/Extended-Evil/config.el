196db847353ae4d21e5a97da6461fabee88c40bb
xin <git@ironshark.org>
2024-06-13 13:16:02 -0400

    Rebuild:  567   2024-06-13 13:13:44

--- Shared/Emacs/Personal-Layers/Extended-Evil/config.el
+++ Shared/Emacs/Personal-Layers/Extended-Evil/config.el
@@ -1,12 +1,12 @@
 (when (configuration-layer/package-used-p 'evil)
 
 
-(global-set-key (kbd "C-x C-s") #'Tn/save-all-buffers)
-(define-key evil-normal-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)
-(define-key evil-hybrid-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)
+;; (global-set-key (kbd "C-x C-s") #'Tn/save-all-buffers)
+;; (define-key evil-normal-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)
+;; (define-key evil-hybrid-state-map (kbd "S-<escape>") 'Tn/evil-normal-and-save)
 
 
-(define-key evil-normal-state-map (kbd "C-h") 'help)
+;; (define-key evil-normal-state-map (kbd "C-h") 'help)
 
 
 
