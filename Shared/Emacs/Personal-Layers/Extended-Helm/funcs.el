43859dc247596e148ca241d1af95bf09904dddd5
xin <git@ironshark.org>
2024-06-13 09:16:53 -0400

    Rebuild:  557   2024-06-13 08:56:16

--- Shared/Emacs/Personal-Layers/Extended-Helm/funcs.el
+++ Shared/Emacs/Personal-Layers/Extended-Helm/funcs.el
@@ -12,10 +12,25 @@
 ; ^ ^
 ; ^ ^
 ;
-; "
-;  ("s" helm-flyspell-correct)
-;  ("e" helm-flycheck)
-;  ("c" helm-company)
-;  ("q" nil "Cancel" :color blue))
+                                        ; "
+                                        ;  ("s" helm-flyspell-correct)
+                                        ;  ("e" helm-flycheck)
+                                        ;  ("c" helm-company)
+                                        ;  ("q" nil "Cancel" :color blue))
 
+  (spacemacs|define-transient-state Tn/helm-actions
+    :title "Helm Actions"
+    :foreign-keys run
+    :doc "
+              ^Helm^
+    ----------------------------
+    _s_: Spell    _e_: Syntax
+    _c_: Company  _o_: Org-Query
+            _q_:  Quit"
+    :bindings
+    ("q" nil :exit t)
+    ("e" helm-flyspell-correct)
+    ("o" helm-org-ql)
+    ("c" helm-company)
+    ("s" helm-flycheck))
 )
