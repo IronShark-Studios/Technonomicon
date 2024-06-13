31d9d172b967085a2cc97294fea8d3778d055ac5
xin <git@ironshark.org>
2024-06-13 08:53:55 -0400

    Rebuild:  555   2024-06-13 08:42:31

--- Shared/Emacs/Personal-Layers/Extended-Helm/funcs.el
+++ Shared/Emacs/Personal-Layers/Extended-Helm/funcs.el
@@ -1,21 +1,21 @@
 (when (configuration-layer/package-used-p 'helm)
 
-;  (require 'hydra)
+  (require 'hydra)
 
-;(eval  '(defhydra Tn/helm-check-hydra (:color blue
-;                                :hint nil)
-;   "
-;            ^Helm^
-;  ----------------------------
-;  _s_: Spell    _e_: Syntax
-;  _c_: Company  _o_: Org-Query
-;  ^ ^
-;  ^ ^
-;
-;  "
-;   ("s" helm-flyspell-correct)
-;   ("e" helm-flycheck)
-;   ("c" helm-company)
-;   ("q" nil "Cancel" :color blue)))
+(defhydra Tn/helm-check-hydra (:color blue
+                               :hint nil)
+  "
+           ^Helm^
+ ----------------------------
+ _s_: Spell    _e_: Syntax
+ _c_: Company  _o_: Org-Query
+ ^ ^
+ ^ ^
+
+ "
+  ("s" helm-flyspell-correct)
+  ("e" helm-flycheck)
+  ("c" helm-company)
+  ("q" nil "Cancel" :color blue))
 
 )
