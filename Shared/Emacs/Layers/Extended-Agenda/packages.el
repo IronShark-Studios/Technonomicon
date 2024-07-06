008640e3f03df1b399d233943761a9066854d2a7
xin <git@ironshark.org>
2024-07-06 07:53:12 -0400

    requires calfw with calfw-org

--- Shared/Emacs/Layers/Extended-Agenda/packages.el
+++ Shared/Emacs/Layers/Extended-Agenda/packages.el
@@ -41,8 +41,8 @@
 ;;; Code:
 
 (defconst Extended-Agenda-packages
-  '(calfw
-    ;;calfw-org
+  '(;;calfw
+    calfw-org
     ;;org-gcal
     org-super-agenda))
 
@@ -50,33 +50,34 @@
   (use-package org-super-agenda
     :defer t))
 
-(defun Extended-Agenda/init-calfw ()
-  "Initialize calfw and add key-bindings"
-  (use-package calfw
-    :defer t
-    :commands (cfw:open-calendar-buffer)
-    :init
-    (spacemacs/set-leader-keys "aCd" 'cfw:open-calendar-buffer)
-    :config
-    (progn
-      (define-key cfw:calendar-mode-map (kbd "SPC") 'spacemacs-cmds)
-      (define-key cfw:calendar-mode-map (kbd "TAB") 'cfw:show-details-command)
-      (define-key cfw:calendar-mode-map (kbd "C-j") 'cfw:navi-next-item-command)
-      (define-key cfw:calendar-mode-map (kbd "C-k") 'cfw:navi-prev-item-command))))
-
-;; (defun Extended-Agenda/init-calfw-org ()
-;;   "Initialize calfw-org and add key-bindings"
-;;   (use-package calfw-org
+;; (defun Extended-Agenda/init-calfw ()
+;;   "Initialize calfw and add key-bindings"
+;;   (use-package calfw
 ;;     :defer t
-;;     :commands (cfw:open-org-calendar)
+;;     :commands (cfw:open-calendar-buffer)
 ;;     :init
-;;     (spacemacs/set-leader-keys "aoCd" 'cfw:open-org-calendar)
+;;     (spacemacs/set-leader-keys "aCd" 'cfw:open-calendar-buffer)
 ;;     :config
 ;;     (progn
-;;       (define-key cfw:org-schedule-map (kbd "SPC") 'spacemacs-cmds)
-;;       (define-key cfw:org-schedule-map (kbd "TAB") 'cfw:org-open-agenda-day)
-;;       (define-key cfw:org-custom-map (kbd "SPC") 'spacemacs-cmds)
-;;       (define-key cfw:org-custom-map (kbd "TAB") 'cfw:org-open-agenda-day))))
+;;       (define-key cfw:calendar-mode-map (kbd "SPC") 'spacemacs-cmds)
+;;       (define-key cfw:calendar-mode-map (kbd "TAB") 'cfw:show-details-command)
+;;       (define-key cfw:calendar-mode-map (kbd "C-j") 'cfw:navi-next-item-command)
+;;       (define-key cfw:calendar-mode-map (kbd "C-k") 'cfw:navi-prev-item-command))))
+
+(defun Extended-Agenda/init-calfw-org ()
+  "Initialize calfw-org and add key-bindings"
+  (use-package calfw-org
+    :defer t
+    :commands (cfw:open-org-calendar)
+    :init
+    (require 'calfw)
+    (spacemacs/set-leader-keys "aoCd" 'cfw:open-org-calendar)
+    :config
+    (progn
+      (define-key cfw:org-schedule-map (kbd "SPC") 'spacemacs-cmds)
+      (define-key cfw:org-schedule-map (kbd "TAB") 'cfw:org-open-agenda-day)
+      (define-key cfw:org-custom-map (kbd "SPC") 'spacemacs-cmds)
+      (define-key cfw:org-custom-map (kbd "TAB") 'cfw:org-open-agenda-day))))
 
 ;; (defun Extended-Agenda/init-org-gcal ()
 ;;   "Initialize org-gcal"
