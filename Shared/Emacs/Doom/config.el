7d9bec3394c3dce8d94bf2d5712e5b66f64a213d
xin <git@ironshark.org>
2024-07-13 07:25:02 -0400

    adds flags to helm configuration

--- Shared/Emacs/Doom/config.el
+++ Shared/Emacs/Doom/config.el
@@ -122,6 +122,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+(setq vc-follow-symlinks t
+      ibuffer-expert t
+      )
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
