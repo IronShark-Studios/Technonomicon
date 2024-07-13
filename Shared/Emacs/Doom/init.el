cd14032a69e4f4af01d280c7a817ade48021a6bf
xin <git@ironshark.org>
2024-07-13 19:02:29 -0400

    Rebuild:   39   2024-07-13 18:42:31

--- Shared/Emacs/Doom/init.el
+++ Shared/Emacs/Doom/init.el
@@ -29,52 +29,53 @@
        :ui
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       (emoji +unicode
-              +ascii
-              +github)  ; 🙂
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       indent-guides     ; highlighted indent columns
+       ;; (emoji +unicode
+       ;;        +ascii
+       ;;        +github)
+                                        ; 🙂
+       ;; hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;; indent-guides  ; highlighted indent columns
        (ligatures +extra) ; ligatures and symbols to make your code pretty again
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
-       ophints           ; highlight the region an operation acts on
-       (popup +defaults) ; tame sudden yet inevitable temporary windows
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       ;; nav-flash         ; blink cursor line after big motions
+       ;; neotree           ; a project drawer, like NERDTree for vim
+       ;; ophints           ; highlight the region an operation acts on
+       ;; (popup +defaults) ; tame sudden yet inevitable temporary windows
+       ;; treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers) ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       ;; workspaces        ; tab emulation, persistence & separate workspaces
+       ;; zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
-       (format +onsave
-               +lsp)  ; automated prettiness
-       lispy             ; vim for lisp, for people who don't like vim
-       multiple-cursors  ; editing in many places at once
-       objed             ; text object editing for the innocent
+       ;; file-templates    ; auto-snippets for empty files
+       ;; fold              ; (nigh) universal code folding
+       ;; (format +onsave
+       ;;       +lsp)  ; automated prettiness
+       ;; lispy             ; vim for lisp, for people who don't like vim
+       ;; multiple-cursors  ; editing in many places at once
+       ;; objed             ; text object editing for the innocent
        ;; parinfer          ; turn lisp into python, sort of
-       rotate-text ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       ;; rotate-text ; cycle region at point between text candidates
+       ;; snippets          ; my elves. They type so I don't have to
+       ;; word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons
-              +dirvish)             ; making dired pretty [functional]
-       electric          ; smarter, keyword-based electric-indent
-       (ibuffer +icons)         ; interactive buffer management
+       ;; (dired +icons
+       ;;      +dirvish)             ; making dired pretty [functional]
+       ;; electric          ; smarter, keyword-based electric-indent
+       ;; (ibuffer +icons)         ; interactive buffer management
        (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       ;; vc                ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;; vterm             ; the best terminal emulation in Emacs
 
        :checkers
        (syntax +childframe
@@ -166,6 +167,7 @@
             +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +brain
+            +contacts
             +dragndrop
             +crypt
             +gnuplot
