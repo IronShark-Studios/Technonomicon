a31ad1cbe795934c25a04628c99ede859576ddd7
xin <git@ironshark.org>
2024-07-21 12:40:08 -0400

    migrates doom configuration

--- Shared/Emacs/emacs.nix
+++ Shared/Emacs/emacs.nix
@@ -1,36 +1,94 @@
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
 
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-unstable-pgtk;
+    extraPackages = epkgs: [ epkgs.vterm ];
   };
 
   home = {
     packages = with pkgs; [
+      (aspellWithDicts (
+        dicts: with dicts; [
+          en
+          en-computers
+          en-science
+        ]
+      ))
+      hunspellDicts.en_US-large
+      hunspell
       fd
       silver-searcher
-      hunspell
-      hunspellDicts.en_US-large
       openscad-lsp
       gforth
       racket
       languagetool
+      python3
+      cmake
+      gnumake
+      ansible
+      docker
+      dockfmt
+      sqlite
+      libclang
+      clojure
+      clojure-lsp
+      cljfmt
+      sbcl
+      glslang
+      libxml2
+      gfortran9
+      fortran-fpm
+      fortls
+      fprettify
+      gdtoolkit_4
+      haskell-language-server
+      ghc
+      cabal-install
+      nixfmt-rfc-style
+      scrot
+      gnuplot
+      zig
+      zls
+      html-tidy
+      stylelint
+      jsbeautifier
+      rustc
+      cargo
+      rust-analyzer
+      shfmt
+      shellcheck
+      pandoc
     ];
 
-    # file = {
-    #   ".spacemacs" = {
-    #     source = config.lib.file.mkOutOfStoreSymlink "/home/xin/Projects/Technonomicon/Shared/Emacs/spacemacs";
-    #   };
+    file = {
+      "config.el" = {
+        source = ./Doom/config.el;
+        target = ".config/doom/config.el";
+      };
 
-    #   ".spacemacs.env" = {
-    #     source = config.lib.file.mkOutOfStoreSymlink "/home/xin/Projects/Technonomicon/Shared/Emacs/spacemacs.env";
-    #   };
+      "init.el" = {
+        source = ./Doom/init.el;
+        target = ".config/doom/init.el";
+      };
 
-    #   "Personal-Layers" = {
-    #     target = ".config/emacs/private";
-    #     source = ./Layers;
-    #     recursive = true;
-    #   };
-    # };
+      "packages.el" = {
+        source = ./Doom/packages.el;
+        target = ".config/doom/packages.el";
+      };
+
+      "secrets.el" = {
+        source = ./secrets.el;
+        target = ".config/doom/secrets.el";
+      };
+    };
   };
 }
