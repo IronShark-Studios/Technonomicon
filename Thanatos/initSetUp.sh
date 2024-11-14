119747f0257f601f5ee5d11fd200a6a258ca99d3
xin <git@ironshark.org>
2024-11-14 12:09:36 -0500

    adds thanatos attempt

---
+++ Thanatos/initSetUp.sh
@@ -0,0 +1,64 @@
+#! /bin/sh
+
+echo "This script is meant to be run after the initial set up"
+echo "of the local system, WebBrowser"
+echo "and encryption keys from Google Drive"
+
+echo "Unlock user secrets with 'git-crypt unlock ~/path/to/key'"
+echo "Please press enter to continue"
+read -r ConfirmationInput
+echo
+
+echo "Configuring Home Directory"
+cd ~
+mkdir ~/.ssh
+mkdir ~/Projects
+mkdir ~/Media
+mkdir ~/G-Drive
+echo
+
+echo "Creating System SSH-Key"
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+echo
+
+echo "Please press enter after configuring git-source SSH."
+read -r ConfirmationInput
+echo
+
+echo "Configuring Personal Archives"
+cd ~
+git clone git@github.com:IronShark-Studios/Apocrypha.git
+git clone git@github.com:IronShark-Studios/Grimoire.git
+cd ~/Projects
+git clone git@github.com:IronShark-Studios/Technonomicon.git
+git clone git@github.com:IronShark-Studios/IronShark-Studios.github.io.git
+mv Iron-Shark.github.io Personal-Blog
+cd ~
+echo
+
+echo
+echo "Home Directory Set Up Complete, configuring R-clone connection"
+echo
+echo "This link must be opened with chromium"
+rclone config create G-Drive drive
+echo
+
+echo "Please press enter to continue"
+echo "after confirming login"
+read -r ConfirmationInput
+echo
+
+echo "Configuring Doom Emacs"
+rm -rf ~/.emacs.d
+mkdir ~/.emacs.d
+mkdir ~/.config/doom
+cd ~/.emacs.d
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d	
+~/.emacs.d/bin/doom install
+emacs --batch -f nerd-icons-install-fonts
+echo
+
+cd ~
+
+echo "Remember to update Tn.Key and hardware file, then rebuild system and reboot"
