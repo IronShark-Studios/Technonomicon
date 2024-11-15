a91eff5668e2469ad1a4a31113670a318817fe18
xin <git@ironshark.org>
2024-11-14 19:20:55 -0500

    adds bluetooth to start up, switches caps lock

--- Thanatos/hyprland.nix
+++ Thanatos/hyprland.nix
@@ -4,7 +4,7 @@
     "hyprland.conf" = {
       target = ".config/hypr/hyprland.conf";
       text = ''
-        exec-once = waybar & mako & emacs --daemon & udiskie -asFN & hyprctl dispatch workspace 1
+        exec-once = waybar & mako & emacs --daemon & udiskie -asFN & blueman-applet
         exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
         exec-once = otd-daemon
 	exec-once = rclone mount G-Drive: ~/G-Drive
