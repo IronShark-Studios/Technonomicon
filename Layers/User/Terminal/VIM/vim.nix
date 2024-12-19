24538172092c4ec3314d837e9d6b5a77be73300c
xin <git@ironshark.org>
2024-12-19 12:47:38 -0500

    Pre-Upgrade: Hades   23   2024-12-16 19:15:10

--- Layers/User/Terminal/VIM/vim.nix
+++ Layers/User/Terminal/VIM/vim.nix
@@ -15,8 +15,22 @@
     set clipboard=unnamedplus
     '';
 
-    plugins = with pkgs.vimPlugins; [
+    extraLuaConfig =''
+    local builtin = require('telescope.builtin')
 
+
+    vim.g.mapleader = " "
+    vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+    vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+    vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>ps", function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+      end)
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+    telescope-nvim
+    nvim-treesitter.withAllGrammars
     ];
   };
 }
