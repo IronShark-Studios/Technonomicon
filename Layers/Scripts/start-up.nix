3e1be8bea713ee368cc16aa4e9bff89836894898
xin <git@ironshark.org>
2025-04-14 21:02:20 -0400

    Rebuild:   15   2025-04-14 20:58:43

--- Layers/Scripts/start-up.nix
+++ Layers/Scripts/start-up.nix
@@ -5,31 +5,13 @@
     text = ''
 #! /bin/sh
 
-hyprctl dispatch workspace 1
-
 zen &
-
-sleep 0.5
-
-hyprctl dispatch workspace 2
 
 obsidian &
 
-sleep 0.5
-
-hyprctl dispatch workspace 8
-
 anki &
 
-sleep 0.5
-
-hyprctl dispatch workspace 7
-
 thunderbird &
-
-sleep 0.5
-
-hyprctl dispatch workspace 9
 
 discord &
     '';
