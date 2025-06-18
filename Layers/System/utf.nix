f560035ac123a15b2d0ca111247f3919c2e1e42f
xin <git@ironshark.org>
2025-06-18 08:58:22 -0400

    Rebuild:   49   2025-06-18 07:40:22

--- Layers/System/utf.nix
+++ Layers/System/utf.nix
@@ -14,12 +14,25 @@
       LC_TELEPHONE = "en_US.UTF-8";
       LC_TIME = "en_US.UTF-8";
     };
-    supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" "ko_KR.UTF-8/UTF-8" ];
+    supportedLocales = [ 
+    "en_US.UTF-8/UTF-8" 
+    "zh_CN.UTF-8/UTF-8" 
+    "ko_KR.UTF-8/UTF-8" 
+    ];
+
     inputMethod = {
-    enabled = "ibus";
-    ibus = {
-      engines = with pkgs.ibus-engines; [ libpinyin hangul ];
-      };
+#    enabled = "ibus";
+#    type = "ibus";
+#    ibus = {
+#      engines = with pkgs.ibus-engines; [ libpinyin hangul ];
+#      };
+    enable = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx-gtk
+	fcitx-chinese-addons
+	fcitx-hangul
+	fcitx-nord
+      ];
     };
   };
 }
