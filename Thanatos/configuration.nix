4f6cc7b7375123d9047c937d7576a58b97d44b98
xin <git@ironshark.org>
2025-06-24 13:22:46 -0400

    Rebuild:   64   2025-06-19 14:53:46

--- Thanatos/configuration.nix
+++ Thanatos/configuration.nix
@@ -77,4 +77,10 @@
       xin = import ./xin-home.nix;
     };
   };
+
+  home.file."fcitx5-config" = {
+    source = ./fcitx5-config;
+    target = .config/fxitx5/config;
+    enable = true;
+  };
 }
