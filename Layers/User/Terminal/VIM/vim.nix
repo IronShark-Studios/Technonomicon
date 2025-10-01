8bb0d6b1b772c8a60cee7b2b286aef344813ce35
xin <git@ironshark.org>
2025-10-01 07:08:19 -0400

    merges old settings into lazyvim :  187   2025-09-30 21:20:18

--- Layers/User/Terminal/VIM/vim.nix
+++ Layers/User/Terminal/VIM/vim.nix
@@ -5,43 +5,11 @@
   };
 
 
-#  programs.nixvim = {
-#    enable = true;
-
-#    viAlias = true;
-#    vimAlias = true;
-#    vimdiffAlias = true;
-
-
-#     extraWrapperArgs = [
-#      "--prefix"
-#      "PATH"
-#      ":"
-#      "${lib.makeBinPath [ pkgs.gcc ]}"
-#    ];
-
-
-#    extraConfig =''
-#    set clipboard=unnamedplus
-#
-#    let fcitx5state=system("fcitx5-remote")
-#autocmd InsertLeave * :silent let fcitx5state=system("fcitx5-remote")[0] | silent !fcitx5-remote -c " Disable the input method when exiting insert mode and save the state
-#autocmd InsertEnter * :silent if fcitx5state == 2 | call system("fcitx5-remote -o") | endif " 2 means that the input method was opened in the previous state, and the input method is started when entering the insert mode
-#
-#    '';
-
- #   extraLuaConfig =''
- #   vim.g.mapleader = " "
- #   '';
-
-#    plugins = with pkgs.vimPlugins; [
-#      nvim-treesitter.withAllGrammars
-#    ];
-#  };
-
-
 programs.neovim = {
   enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
     extraPackages = with pkgs; [
       # LazyVim
       lua-language-server
@@ -53,6 +21,13 @@ programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       lazy-nvim
     ];
+
+    extraConfig =''
+      let fcitx5state=system("fcitx5-remote")
+      autocmd InsertLeave * :silent let fcitx5state=system("fcitx5-remote")[0] | silent !fcitx5-remote -c " Disable the input method when exiting insert mode and save the state
+      autocmd InsertEnter * :silent if fcitx5state == 2 | call system("fcitx5-remote -o") | endif " 2 means that the input method was opened in the previous state, and the input method is started when entering the insert mode
+
+    '';
 
     extraLuaConfig =
       let
