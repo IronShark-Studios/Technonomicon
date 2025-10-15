44889aab54df7e84753ab060d471ab380f1fd92d
xin <git@ironshark.org>
2025-10-15 18:20:13 -0400

    test :  292   2025-10-15 18:05:37

--- Layers/User/Terminal/VIM/Lua/config/autocmds.lua
+++ Layers/User/Terminal/VIM/Lua/config/autocmds.lua
@@ -25,7 +25,7 @@ end, {
 
 vim.api.nvim_create_user_command("Ne", function()
 	local shell_cmd =
-		'! export current_dir=$(pwd) && hyprctl dispatch exec "alacritty --working-directory $current_dir -e "rofi -show file-browser-extended""'
+		'! export current_dir=$(pwd) && hyprctl dispatch exec "rofi -show file-browser-extended -file-browser-dir $current_dir"'
 	vim.cmd(shell_cmd)
 end, {
 	desc = "Open a new Ranger terminal in the current directory",
