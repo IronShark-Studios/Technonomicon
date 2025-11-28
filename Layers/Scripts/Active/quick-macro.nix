5c0aaef9ec50a7f4391b28dab04240cd624d8676
xin <git@ironshark.org>
2025-11-28 13:04:50 -0500

    Pre-Upgrade: Thanatos  379   2025-11-23 08:33:12

--- Layers/Scripts/Active/quick-macro.nix
+++ Layers/Scripts/Active/quick-macro.nix
@@ -4,6 +4,15 @@
     target = "scripts/quick-macro.sh";
     text = ''
       #!/bin/sh
+
+      sleep 1.5
+      ydotool key 97:1 46:1 46:0 97:0
+      sleep 1.5
+      ydotool key 125:1 105:1 105:0 125:0
+      sleep 1.5
+      ydotool key 97:1 42:1 47:1 47:0 42:0 97:0
+      # sleep 1.5
+      # ydotool key 97:1 49:1 49:0 97:0
     '';
   };
 }
