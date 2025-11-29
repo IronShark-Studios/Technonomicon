f91d6f57c0be400fc91c9cf875bb0e9a567744ff
xin <git@ironshark.org>
2025-11-28 21:11:57 -0500

    adds quick script :  379   2025-11-23 08:33:12

--- Layers/Scripts/Active/quick-macro.nix
+++ Layers/Scripts/Active/quick-macro.nix
@@ -5,14 +5,16 @@
     text = ''
       #!/bin/sh
 
-      sleep 1.5
+      sleep 0.5
       ydotool key 97:1 46:1 46:0 97:0
-      sleep 1.5
+      sleep 0.25
       ydotool key 125:1 105:1 105:0 125:0
-      sleep 1.5
+      sleep 0.25
       ydotool key 97:1 42:1 47:1 47:0 42:0 97:0
-      # sleep 1.5
-      # ydotool key 97:1 49:1 49:0 97:0
+      sleep 0.25
+      ydotool key 97:1 49:1 49:0 97:0
+      sleep 0.25
+      ydotool key 97:1 46:1 46:0 97:0
     '';
   };
 }
