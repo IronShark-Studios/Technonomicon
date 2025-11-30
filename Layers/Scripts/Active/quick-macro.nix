c1a553f263717e5128706c3cdfc9fe31dc77e3d0
xin <git@ironshark.org>
2025-11-30 07:40:15 -0500

    changed quick macro for madarin :  382   2025-11-28 21:11:55

--- Layers/Scripts/Active/quick-macro.nix
+++ Layers/Scripts/Active/quick-macro.nix
@@ -5,16 +5,8 @@
     text = ''
       #!/bin/sh
 
-      sleep 0.5
-      ydotool key 97:1 46:1 46:0 97:0
-      sleep 0.25
-      ydotool key 125:1 105:1 105:0 125:0
-      sleep 0.25
-      ydotool key 97:1 42:1 47:1 47:0 42:0 97:0
-      sleep 0.25
+      sleep 0.35
       ydotool key 97:1 49:1 49:0 97:0
-      sleep 0.25
-      ydotool key 97:1 46:1 46:0 97:0
     '';
   };
 }
