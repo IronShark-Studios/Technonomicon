e193b4dc7a055f42df30567da1811037cb2a700c
Xin <git@ironshark.org>
2026-03-07 23:29:56 -0500

    post upgrade

--- Layers/System/systemd.nix
+++ Layers/System/systemd.nix
@@ -1,8 +1,8 @@
 { inputs, outputs, lib, config, pkgs, ...}: {
 
-  systemd.sleep.extraConfig = ''
-    HandleSuspend=ignore
-  '';
+  systemd.sleep.settings.Sleep = {
+    HandleSuspend = "ignore";
+  };
 
   systemd.services.ModemManager.enable = false;
 }
