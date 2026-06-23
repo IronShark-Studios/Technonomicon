93c775ee9a838f4450a6d0e02f30668ded0badf9
xin <git@ironshark.org>
2026-06-23 17:13:09 -0400

    auto: update communication.nix

--- Modules/Web/communication.nix
+++ Modules/Web/communication.nix
@@ -1,30 +1,7 @@
 { inputs, ... }: {
   flake.nixosModules.Tn-communication = { pkgs, ... }: {
 
-    hjem.users.xin = {
-      enable = true;
-      files = {
-        ".config/discord/settings.json".text = builtins.toJSON {
-          SKIP_HOST_UPDATE = true;
-        };
-        ".config/mako/config".text = ''
-          default-timeout=3000
-        '';
-      };
-    };
-
-    systemd.user.services.mako = {
-      description = "Mako notification daemon for EWM layer-shell support";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.mako}/bin/mako";
-        Restart = "on-failure";
-      };
-    };
-
     environment.systemPackages = with pkgs; [
-      #  slack
-      #  discord
       newsflash
       mako
     ];
