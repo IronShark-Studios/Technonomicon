9f6e2b7b694d359e335622ee8a85168e69ecb401
xin <git@ironshark.org>
2026-06-23 17:12:03 -0400

    auto: update shell.nix

--- Modules/Core/shell.nix
+++ Modules/Core/shell.nix
@@ -40,15 +40,6 @@
       nix-direnv.enable = true;
     };
 
-    environment.etc."gitconfig".source = ./_gitconfig;
-    environment.etc."gitignore_global".text = ''
-      *~
-    .*~
-    #*#
-    \#*\#
-    .*.swp
-    '';
-
     programs.starship = {
       enable = true;
       settings = {
@@ -66,7 +57,7 @@
           truncate_to_repo = true;
           fish_style_pwd_dir_length = 3;
           format = "[$read_only$path]($style) ";
-          read_only = " ";
+          read_only = " ";
         };
 
         git_branch = {
@@ -88,15 +79,15 @@
           style = "#539bf5";
           format = "[$conflicted$staged$modified$renamed$deleted$untracked$stashed$ahead_behind]($style) ";
 
-          conflicted = "[ ](bold fg:#539bf5)";
-          staged = "[ ](fg:#539bf5)";
-          modified = "[ ](fg:#539bf5)";
-          renamed = "[ ](fg:#539bf5)";
-          deleted = "[ ](fg:#539bf5)";
+          conflicted = "[ ](bold fg:#539bf5)";
+          staged = "[ ](fg:#539bf5)";
+          modified = "[ ](fg:#539bf5)";
+          renamed = "[ ](fg:#539bf5)";
+          deleted = "[ ](fg:#539bf5)";
           untracked = "[? ](fg:#539bf5)";
-          stashed = "[ ](fg:#539bf5)";
-          ahead = "[ ](fg:#539bf5)";
-          behind = "[ ](fg:#539bf5)";
+          stashed = "[ ](fg:#539bf5)";
+          ahead = "[ ](fg:#539bf5)";
+          behind = "[ ](fg:#539bf5)";
         };
       };
     };
@@ -148,9 +139,12 @@
       aria2
       bzip3
       nemo-with-extensions
-      # bitwarden-cli
-      # bitwarden-desktop
       nvtopPackages.full
+      zellij
+      yazi
+      glow
+      presenterm
+      frogmouth
     ];
   };
 }
