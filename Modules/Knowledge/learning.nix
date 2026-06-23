73a33092f790596ba3dfba8e8b226b7aeeebe463
xin <git@ironshark.org>
2026-06-23 17:13:14 -0400

    auto: update learning.nix

--- Modules/Knowledge/learning.nix
+++ Modules/Knowledge/learning.nix
@@ -1,39 +1,19 @@
 { inputs, ... }: {
-  flake.nixosModules.Tn-learning = { pkgs, config, ... }: {
+  flake.nixosModules.Tn-learning = { pkgs, ... }: {
 
     environment.systemPackages = with pkgs; [
       ledger
       fava
       beancount
       gnucash
-      visidata
-      anki-bin
-      zotero
-      zathura
-      libation
-      pdfannots2json
       (symlinkJoin {
         name = "rnote-wrapped";
         paths = [ rnote ];
         buildInputs = [ makeWrapper ];
         postBuild = ''
           wrapProgram $out/bin/rnote \
-        --set GDK_BACKEND x11
+            --set GDK_BACKEND x11
         '';
-      })
-    ];
-
-    services.udev.packages = [
-      (pkgs.writeTextFile {
-        name = "javelin-udev-rules";
-        text = ''
-          # RP2040 Bootloader (for flashing firmware)
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="0003", TAG+="uaccess"
-
-      # Generic HID (for Javelin WebHID Tools)
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess"
-        '';
-        destination = "/etc/udev/rules.d/70-javelin.rules";
       })
     ];
 
