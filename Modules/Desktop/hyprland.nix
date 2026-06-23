1f0256b513c7887b6367b3346c974749679fff94
xin <git@ironshark.org>
2026-06-23 17:08:18 -0400

    auto: update hyprland.nix

---
+++ Modules/Desktop/hyprland.nix
@@ -0,0 +1,44 @@
+{ inputs, ... }: {
+  flake.nixosModules.Tn-hyprland = { pkgs, ... }: {
+    imports = [ inputs.hyprland.nixosModules.default ];
+
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      plugins = [
+        pkgs.hyprlandPlugins.hyprscrolling
+        pkgs.hyprlandPlugins.hyprexpo
+      ];
+    };
+
+    security.pam.services.hyprlock = {};
+
+    environment.systemPackages = with pkgs; [
+      hyprlock
+      hypridle
+      hyprpaper
+      waybar
+      anyrun
+      wl-kbptr
+      grim
+      slurp
+      wl-clipboard
+      wtype
+      hyprpicker
+      satty
+      wf-recorder
+      awww
+      wlogout
+      nwg-look
+      hyprshot
+    ];
+
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      QT_QPA_PLATFORM = "wayland";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      NIXPKGS_ALLOW_UNFREE = "1";
+    };
+  };
+}
