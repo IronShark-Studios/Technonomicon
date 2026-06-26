ff56e70e4ca4bbcc20c8b1e6cec024b4cc483dd9
xin <git@ironshark.org>
2026-06-26 18:27:23 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -2,6 +2,7 @@
   flake.nixosModules.Tn-hyprland = { pkgs, ... }: {
 
     programs.hyprland.enable = true;
+    programs.hyprland.plugins = [ pkgs.hyprlandPlugins.hypr-dynamic-cursors ];
     programs.hyprlock.enable = true;
 
     xdg.portal.extraPortals  = [ pkgs.xdg-desktop-portal-hyprland ];
