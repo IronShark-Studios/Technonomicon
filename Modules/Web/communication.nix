96eabe2aee77fd5d4b5b15ba45a47d8b6aea955a
xin <git@ironshark.org>
2026-06-26 18:19:45 -0400

    auto: update communication.nix

--- Modules/Web/communication.nix
+++ Modules/Web/communication.nix
@@ -1,8 +1,21 @@
 { inputs, ... }: {
   flake.nixosModules.Tn-communication = { pkgs, ... }: {
 
+    imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
+
     home-manager.users.xin.home.file.".config/discord/settings.json".text = builtins.toJSON {
       SKIP_HOST_UPDATE = true;
+    };
+
+    services.flatpak = {
+      enable = true;
+      remotes = [{
+        name     = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }];
+      packages = [
+        { appId = "com.discordapp.Discord"; origin = "flathub"; }
+      ];
     };
 
     environment.systemPackages = with pkgs; [
