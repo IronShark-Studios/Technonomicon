41daa41bf0cf9c395a00eb65b0bfbd0d86a016cd
xin <git@ironshark.org>
2026-06-26 18:23:15 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -74,23 +74,6 @@
             scrolling = {
               fullscreen_on_one_column = true,
             },
-            plugin = {
-              ["hypr-dynamic-cursors"] = {
-                enabled   = true,
-                threshold = 2,
-                shake = {
-                  enabled  = true,
-                  factor   = 1.5,
-                  base     = 0.5,
-                  slowdown = 0.85,
-                },
-                hide = {
-                  enabled   = true,
-                  hideDelay = 0,
-                  showDelay = 0,
-                },
-              },
-            },
           })
 
           hl.bind(mainMod .. " + Return",    hl.dsp.exec_cmd("anyrun"))
