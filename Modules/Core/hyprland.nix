e7680fca981612dbedf44fea8306d4199a7050ef
xin <git@ironshark.org>
2026-06-26 19:43:00 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -69,7 +69,7 @@
             input = {
               kb_layout    = "us",
               follow_mouse = 1,
-              touchpad     = { natural_scroll = true },
+              touchpad     = { natural_scroll = true, disable_while_typing = true },
             },
             general = {
               gaps_in     = 5,
