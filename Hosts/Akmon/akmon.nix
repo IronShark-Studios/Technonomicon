6e8f518b59868acf3be35a8197cf39c8aa6f3399
xin <git@ironshark.org>
2026-06-26 15:09:46 -0400

    auto: update akmon.nix

--- Hosts/Akmon/akmon.nix
+++ Hosts/Akmon/akmon.nix
@@ -18,6 +18,7 @@
 
       self.nixosModules.Tn-nix
       self.nixosModules.Tn-desktop
+      self.nixosModules.Tn-hyprland
       self.nixosModules.Tn-helix
       self.nixosModules.Tn-web-browsers
       self.nixosModules.Tn-web-apps
