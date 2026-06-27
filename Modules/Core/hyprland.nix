17203583fc2ccb4844ea5397012a15be77f16b26
xin <git@ironshark.org>
2026-06-27 07:10:25 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -67,6 +67,7 @@
             hl.exec_cmd("nm-applet --indicator")
             hl.exec_cmd("udiskie --tray")
             hl.exec_cmd("blueman-applet")
+            hl.exec_cmd("pasystray")
             hl.exec_cmd("${startupApps}")
           end)
 
